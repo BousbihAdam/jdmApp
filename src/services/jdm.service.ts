f793774ec9a99484c65efc7bb9f028686999f3de
Bousbih Adam <adam@MacBook-Pro-de-Bousbih.local>
2019-01-19 03:54:09 +0100

    fiel

--- src/services/jdm.service.ts
+++ src/services/jdm.service.ts
@@ -7,7 +7,7 @@ export class JdmService {
   constructor(private http: Http) { }
 
   getResult(term: String){
-    return this.http.get('http://localhost:3000/'+term).map(result => result.json());
+    return this.http.get('https://jdm-server.herokuapp.com/'+term).map(result => result.json());
   }
 
 }
